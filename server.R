32bbcaa4ad539af374997d015a9f99da9289525c
royr2 <royr2@users.noreply.github.com>
2024-02-15 19:55:39 +0530

    Initial commit

---
+++ server.R
@@ -0,0 +1,61 @@
+# Define server logic required to draw a histogram
+function(input, output, session) {
+  
+  # Read file and update inputs 
+  observeEvent(input$input_file, {
+    model_data <<- read.csv(input$input_file$datapath)
+    model_data <<- as.data.frame(model_data)
+    
+    # Update input section
+    updateSelectInput(session, inputId = "covariates", choices = colnames(model_data), selected = NULL)
+    updateSelectInput(session, inputId = "dependent", choices = colnames(model_data), selected = NULL)
+  })
+  
+  # Reactive expressions
+  engine <<- reactive({input$dtree_engine})
+  
+  # Fit Model
+  observeEvent(input$run_model, {
+    
+    form <<- paste0(input$dependent, "~", paste0(input$covariates, collapse = "+"))
+    
+    if(engine() == "RPART"){
+      mdl <<- rpart(as.formula(form), 
+                    data = model_data, 
+                    control = rpart.control(minbucket = input$par_minbucket, 
+                                            cp = input$par_cp, 
+                                            maxdepth = input$par_maxdepth))
+      
+      output$tree_plot <- renderPlot({
+        rpart.plot(mdl, main = paste("Segmentation via", engine()))
+      })
+    }
+    
+    if(engine() == "CTREE"){
+      rec <- recipe(model_data, formula = form) %>% 
+        step_string2factor(all_string()) %>%
+        step_dummy(all_factor()) %>% 
+        prep()
+      
+      model_data_ctree <- bake(rec, new_data = NULL) %>% 
+        mutate(!! sym(input$dependent) := as.factor(!! sym(input$dependent)))
+      
+      form <<- paste0(input$dependent, "~ .")
+      
+      mdl <<- ctree(as.formula(form), 
+                    data = model_data_ctree, 
+                    control = ctree_control(minbucket = input$par_minbucket, 
+                                            maxdepth = input$par_maxdepth, 
+                                            mincriterion = input$par_mincriterion))
+      
+      output$tree_plot <- renderPlot({
+        plot.party(mdl, main = paste("Segmentation via", engine()))
+      })
+    }
+    
+  })
+  
+  
+  
+  
+}
---
+++ ui.R
@@ -0,0 +1,39 @@
+fluidPage(
+  
+  # Application title
+  titlePanel("Segmentation"),
+  
+  # Sidebar with a slider input for number of bins
+  fluidRow(
+    
+    column(2, 
+           wellPanel(
+             
+             # File input
+             fileInput("input_file", "Choose CSV File", accept = ".csv"),
+             
+             # DTREE Engine
+             selectInput("dtree_engine", "Select D-Tree Engine", c("RPART", "CTREE"), "RPART"),
+             
+             # Model inputs
+             selectInput("covariates", "X-Vars", choices = NULL, selected = NULL, multiple = T, selectize = T),
+             selectInput("dependent", "Y-Var", choices = NULL, selected = NULL, multiple = F, selectize = T),
+             
+             # Parameters
+             tags$b("Model Parameters:"),
+             tags$hr(),
+             
+             numericInput("par_minbucket", "Minbucket", value = 100),
+             numericInput("par_maxdepth", "Maxdepth", value = 3),
+             numericInput("par_cp", "Cp (RPART)", value = -1),
+             numericInput("par_mincriterion", "Mincriterion (CTREE)", value = 0.95),
+             
+             # Model run
+             actionButton("run_model", "Fit Model")
+           )
+    ),
+    
+    # Plot output
+    column(10, plotOutput("tree_plot"))
+  )
+)
